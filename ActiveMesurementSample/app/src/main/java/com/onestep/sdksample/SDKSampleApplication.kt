6fad916d471143996dccab22022c6b10f20d8e60
Shahar Davidson <shahar@onestep.co>
2024-07-28 14:26:31 +0300

    removing BG related things, making this init as thin as possible for In-App feature;
    Add some user profile demographic for adjusted norms;

--- ActiveMesurementSample/app/src/main/java/com/onestep/sdksample/SDKSampleApplication.kt
+++ ActiveMesurementSample/app/src/main/java/com/onestep/sdksample/SDKSampleApplication.kt
@@ -6,19 +6,19 @@ import co.onestep.android.core.external.OneStep
 import co.onestep.android.core.external.models.InitResult
 import co.onestep.android.core.external.models.NotificationConfig
 import co.onestep.android.core.external.models.SdkConfiguration
-import co.onestep.android.core.internal.data.syncer.WalksSyncScheduler
+import co.onestep.android.core.external.models.UserAttributes
+import co.onestep.android.core.internal.utils.ISO_FORMAT
+import co.onestep.android.core.internal.utils.toDate
 import kotlinx.coroutines.flow.MutableSharedFlow
 
 class SDKSampleApplication: Application() {
-
-    private val TAG: String? = SDKSampleApplication::class.simpleName
 
     val sdkConnectionState = MutableSharedFlow<InitResult>(1)
 
     override fun onCreate() {
         super.onCreate()
         connect {
-            Log.d(TAG, "connection result $it")
+            Log.d("SDKSampleApplication", "connection result $it")
             sdkConnectionState.tryEmit(it)
         }
     }
@@ -35,27 +35,30 @@ class SDKSampleApplication: Application() {
         )
             .setConfiguration(
                 SdkConfiguration(
-                    // syncConfigurations is an enum value that you can set
-                    // to @Enhanced @Balanced or @Efficient
-                    syncConfigurations = WalksSyncScheduler.SyncConfigurations.Enhanced,
+                    // SDK will be used only for in-app recording
+                    enableMonitoringFeature = false,
+                    // use the mock IMU for testing in emulator
+                    mockIMU = false,
                 ),
             )
-            // implement the AnalyticsHandler interface to receive analytics events
-            .setAnalyticsService(SampleAnalytics())
-            // set the foreground notification configuration attributes for the background data collection
-            .setBackgroundNotificationConfig(
-                NotificationConfig(
-                    title = "This is the Demo App recording",
-                    icon = R.drawable.ic_launcher_foreground,
-                ),
+            // set the user profile attributes
+            .setUserAttributes(
+                UserAttributes.Builder()
+                    .withFirstName("John")
+                    .withLastName("Doe")
+                    .withSex(UserAttributes.Gender.MALE.description)
+                    .withDateOfBirth("1977-05-25".toDate(dateFormat = ISO_FORMAT))
+                    .build()
             )
-            // set the foreground notification configuration attributes for the active measurements
+            // customize the foreground notification for active motion recorder
             .setInAppNotificationConfig(
                 NotificationConfig(
                     title = "This is the Demo App recording",
                     icon = R.drawable.ic_launcher_foreground,
                 ),
             )
+            // implement the AnalyticsHandler interface to receive analytics events
+            .setAnalyticsService(SampleAnalytics())
             .setInitializationCallback {
                 onConnectionResult(it)
             }
